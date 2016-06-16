74d678b519d086bf22448b4cc0ad07fc14472c9b
Shriprasad Patil <Shriprasad.Patil@Mvtechlabs.com>
2016-06-16 15:09:21 +0530

    Changes in Static

--- src/com/first/HelloWorld.java
+++ src/com/first/HelloWorld.java
@@ -2,9 +2,14 @@ package com.first;
 
 public class HelloWorld {
 
+	static
+	{
+		System.out.println("This will executed first");
+	}
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to git");
+		System.out.println("Changes occured here");
 	}
 
 }
