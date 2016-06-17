6d2f6da8a0e219caf1a917c1434ca614271ad587
Dhananjay MorePatil <Dhananjay.MorePatil@Mvtechlabs.com>
2016-06-17 16:31:09 +0530

    supriya

--- src/com/first/HelloWorld.java
+++ src/com/first/HelloWorld.java
@@ -7,21 +7,9 @@ public class HelloWorld {
 		System.out.println("This will executed first");
 	}
 	public static void main(String[] args) {
-
-
-		System.out.println("Welcome to git");
-		System.out.println("Changes occured here");
 		
-		
-		System.out.println("Another changes occured here ");
-		
-		System.out.println("Third Change");
-		
-		
-		System.out.println("fourth Change");
-
-		System.out.println("Welcome to Parkar");
-		System.out.println("Shree");
+		for(int i=0;i<10;i++)
+		System.out.println("More Patil");
 
 	}
 }
